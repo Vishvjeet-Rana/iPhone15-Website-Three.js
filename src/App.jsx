123c7c8f37cad4681cc2022228215529f1379eff
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-01-31 15:39:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="text-center ">
+        <h1 className="underline text-6xl font-extrabold text-yellow-400">
+          Hello world this is tailwind setup
+        </h1>
+        <h2 className=" text-6xl underline font-extrabold text-this_is_teal">
+          wah wah kya baat hai
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
