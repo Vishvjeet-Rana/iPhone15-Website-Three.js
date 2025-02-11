b1cf7cf845fc13950edffb9055b07de80892f151
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-02-11 12:59:03 +0530

    all basic setup done. moving forward

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
 function App() {
-  return (
-    <>
-      <div className="text-center ">
-        <h1 className="underline text-6xl font-extrabold text-yellow-400">
-          Hello world this is tailwind setup
-        </h1>
-        <h2 className=" text-6xl underline font-extrabold text-this_is_teal">
-          wah wah kya baat hai
-        </h2>
-        <h3>hello</h3>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
