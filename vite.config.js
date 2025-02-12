fcdd05ebd38440cae3047aa4c5216c1fef35f95b
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-02-12 19:49:35 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "gsap_with_threejs", // Add a leading and trailing slash
+  base: "/gsap_with_threejs", // Add a leading and trailing slash
   build: {
     outDir: "./docs", // Specify the output directory
   },
